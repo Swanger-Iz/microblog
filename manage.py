bad3dade756745abf30c1d34119f1043c4cfa076
Ilgiz Magadiev <swanger789@gmail.com>
2021-02-08 12:30:09 +0300

    Configured flask shell, added flask-login

--- manage.py
+++ manage.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 # Учебник - https://habr.com/en/post/346306/
+ 
+from app import app, db
+from app.models import User, Post
 
-from app import app
+@app.shell_context_processor
+def make_shell_context():
+    print('Started')
+    return {'db': db, 'User': User, 'Post': Post}
